7933d3c9ff6f61c7ffd7482fa8e492f7168ac44e
Jaenudin Firdaus <2022110103@students.uigm.ac.id>
2025-05-08 10:59:03 +0700

    Install and use cors

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import express from "express";
 import productRoutes from "./routes/product.routes.js";
-
+import cors from "cors";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(cors());
 
 app.use("/api/products", productRoutes);
 
